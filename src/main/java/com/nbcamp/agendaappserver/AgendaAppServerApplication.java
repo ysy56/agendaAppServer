e5e32a13f45732338dfceec6a15f746402721d5e
moony <78634780+ysy56@users.noreply.github.com>
2024-05-17 01:47:46 +0900

    add[Initial] : 초기 세팅

---
+++ src/main/java/com/nbcamp/agendaappserver/AgendaAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.nbcamp.agendaappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaAppServerApplication.class, args);
+    }
+
+}
