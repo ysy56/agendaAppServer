cc747cb760657f6a5cb77cb79c61c1d1619350e9
moony <78634780+ysy56@users.noreply.github.com>
2024-05-28 17:05:03 +0900

    feat[1단계] : 일정과 댓글의 연관 관계

--- src/main/java/com/nbcamp/agendaappserver/entity/Comment.java
+++ src/main/java/com/nbcamp/agendaappserver/entity/Comment.java
@@ -1,9 +1,13 @@
 package com.nbcamp.agendaappserver.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
@@ -14,4 +18,24 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "content")
+    private String content;
+    @Column(name = "manager")
+    private String manager;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.DATE)
+    private LocalDate writedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "agenda_id")
+    private Agenda agenda;
+
+    @Builder
+    public Comment(String content, String manager, Agenda agenda) {
+        this.content = content;
+        this.manager = manager;
+        this.agenda = agenda;
+    }
 }
