5a4206777a2178c0a7e8ebc8406773b4b878583f
moony <78634780+ysy56@users.noreply.github.com>
2024-05-29 15:50:50 +0900

    feat[5단계]: 쿠키 세션 방식 틀 추가

--- src/main/java/com/nbcamp/agendaappserver/exception/AgendaControllerAdvice.java
+++ src/main/java/com/nbcamp/agendaappserver/exception/AgendaControllerAdvice.java
@@ -1,6 +1,5 @@
-package com.nbcamp.agendaappserver.controller;
+package com.nbcamp.agendaappserver.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
